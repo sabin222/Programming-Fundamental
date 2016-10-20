426c84cc4b7f0572bc4378c95fb79a82014d1ccc
sabin222 <cabinkoju@gmail.com>
2016-10-20 12:12:57 +0545

    Add files via upload
    Relation Operator

---
+++ operator123.c
@@ -0,0 +1,39 @@
+/*
+Name: sabin koju
+Roll:
+Subject:Programming fundamental
+Lab: 04
+Programme: Program to use of Assignment operator
+Date:20 oct,2016
+*/
+
+#include<stdio.h>
+int main()
+
+{
+	int a,b;
+	printf("\n enter first Number");
+	scanf("%d",&a);
+	printf("\n Enter second Number");
+	scanf("%d",&b);
+	
+	a==b;
+printf("\n in use of == of %d",a==b);
+
+a>b;
+printf("\n in use of > of %d",a>b);
+
+a<b;
+printf("\n in use of < of %d",a<b);
+
+a!=b;
+printf("\n in use of != of %d",a!=b);
+
+a>=b;
+printf("\n in use of>= of %d",a>=b);
+
+a<=b;
+printf("\n in use of <= of %d",a<=b);
+
+return 0;
+}
