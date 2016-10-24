f90f51fccbd7f75a23f6c150d534c491b278e519
Sabin koju <cabinkoju@gmail.com>
2016-10-24 10:48:04 +0545

    Add files via upload
    Write a c-program to check number odd or even using terniary operator.

---
+++ odd_and_even.c
@@ -0,0 +1,19 @@
+/*
+Name: Sabin koju
+Roll:
+Subject: Programming fundamental
+Program: Write a c-program to check number odd or even using terniary operator. 
+Date:24 oct,2016
+*/
+
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\n Enter number :");
+scanf("%d",&a);
+
+a%2==0?printf("\n\n Given number is even :%d",a):printf("\n\n Given number is even :%d",a);
+
+return 0;
+}
