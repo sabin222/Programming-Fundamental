c496728803737b7d95f837720451f58eee3f53e3
Sabin koju <cabinkoju@gmail.com>
2016-11-14 10:37:24 +0545

    Add files via upload
    Write a program to print name from user.

---
+++ Ask_name_from_user.c
@@ -0,0 +1,25 @@
+/*
+Name : Sabin Koju
+Subject : Programming fundamental
+Lab No : 01
+Program : Write a program to print name from user.
+Date :14 Nov 2016
+Roll:
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+char name [50];
+printf("\n\t\t 009900+++++++==========+++++++009900");
+printf("\n\t\t 009900+++++++ WELCOME +++++++ 009900\n\n");
+printf("\n\t\t\t Enter Your Name : ");
+scanf("%s",&name);
+printf("\n\t\t\t Your Name is %s\n",name);
+printf("\n\t\t 009900+++++++========== +++++++009900");
+printf("\n\t\t 009900+++++++ THANK-YOU +++++++009900\n\n\n\n");
+
+return 0;
+}
