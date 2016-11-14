41a3209dbd11e2020d61a7a9f62e0e5044732732
Sabin koju <cabinkoju@gmail.com>
2016-11-14 10:38:31 +0545

    Add files via upload
    Write a program to print your name.

---
+++ print_name.c
@@ -0,0 +1,22 @@
+/*
+Name : Sabin Koju
+Subject : Programming fundamental
+Lab No : 01
+Program : Write a program to print your name.
+Date :14 Nov 2016
+Roll:
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+printf("\n\t\t 009900+++++++==========+++++++009900");
+printf("\n\t\t 009900+++++++ WELCOME +++++++ 009900\n\n");
+printf("\n\t\t\t Welcome Sabin...\n\n");
+printf("\n\t\t 009900+++++++========== +++++++009900");
+printf("\n\t\t 009900+++++++ THANK-YOU +++++++009900\n\n\n\n");
+
+return 0;
+}
