8a056084fd3690d5b02d8a8310a91cdced7135a8
Sabin koju <cabinkoju@gmail.com>
2016-11-16 10:34:20 +0545

    Add files via upload
     Write a c-program to print sum of two numbers.

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,27 @@
+/*
+Name : Sabin Koju
+Subject : Programming fundamental
+Roll : 12
+Lab sheet No : 02
+Program : Write a c-program to print sum of two numbers.
+Date : 16 Nov 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+int a,b,sum;
+printf("\n\t\t\t --==+++++333*****444*****555---12222--");
+printf("\n\t\t\t --==+++++333** WELCOME **555---12222--\n\n");
+printf("\n\n\t\t Enter the first Number :  ");
+scanf("%d",&a);
+printf("\n\n\t\t Enter the Second Number :  ");
+scanf("%d",&b);
+sum=a+b;
+printf("\n\n\n\n\t\t The sum of Two number is :%d \n\n",sum);
+printf("\n\t\t\t --==+++++333***** END *****555---2222--");
+printf("\n\t\t\t --==+++++1333*****444*****555---12222--");
+//getch();
+return 0;
+}
