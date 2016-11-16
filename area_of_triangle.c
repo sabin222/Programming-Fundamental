cddb836a093ac751c80d6c58d5a78e70fc77b172
Sabin koju <cabinkoju@gmail.com>
2016-11-16 12:41:18 +0545

    Add files via upload
    Write a c-program to print area of triangle hight and base asked from user.

---
+++ area_of_triangle.c
@@ -0,0 +1,28 @@
+/*
+Name : Sabin Koju
+Subject : Programming fundamental
+Roll : 12
+Lab sheet No : 02
+Program : Write a c-program to print area of triangle hight and base asked from user.
+Date : 16 Nov 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+float a,b,A;
+
+printf("\n\t\t\t --==+++++11333+++4444+++555---112222--");
+printf("\n\t\t\t --==+++++333** WELCOME **555---12222--\n\n");
+printf("\n\t Enter hight of triangle : ");
+scanf("%f",&a);
+printf("\n\t Enter Base of triangle : ");
+scanf("%f",&b);
+A=(b*a)/2;
+printf("\n\n\t Area of triangle is : %f",A);
+
+getch();
+return 0;
+}
