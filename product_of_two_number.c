204258d65191dce2df6ca6e3f1ee61519c0342af
Sabin koju <cabinkoju@gmail.com>
2016-11-16 10:33:54 +0545

    Add files via upload
    Write a c-program to print product of two numbers.

---
+++ product_of_two_number.c
@@ -0,0 +1,30 @@
+/*
+Name : Sabin Koju
+Subject : Programming fundamental
+Roll : 12
+Lab sheet No : 02
+Program : Write a c-program to print product of two numbers.
+Date : 16 Nov 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int a,b,product;
+
+printf("\n\t\t\t --==+++++11333+++4444+++555---112222--");
+printf("\n\t\t\t --==+++++333** WELCOME **555---12222--\n\n");
+printf("\n\n\t\t Enter the first Number : ");
+scanf("%d",&a);
+printf("\n\n\t\t Enter the Second Number : ");
+scanf("%d",&b);
+product=a*b;
+printf("\n\n\t\t The product of Two number is :%d \n\n",product);
+printf("\n\t\t\t --==+++++11333+++4444+++555---112222--");
+printf("\n\t\t\t --==+++++11333+++ END +++555---12222--\n\n");
+
+getch();
+return 0;
+}
