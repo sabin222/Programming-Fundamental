06af4d35a62a9c33155a52bfb6635f0c60bc95e0
Sabin koju <cabinkoju@gmail.com>
2016-11-23 10:56:23 +0545

    Add files via upload
    WPA to find roots of Quadratic eqn Value assigned by user.

---
+++ Quadratic_equation_value.c
@@ -0,0 +1,31 @@
+/*
+Name : Sabin Koju
+Subject : Programming fundamental
+Lab No : 01
+Program : WPA to find roots of Quadratic eqn Value assigned by user.
+Date :14 Nov 2016
+Roll:
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main ()
+{
+float x1,x2,a,b,c;
+printf("\n Enter the value of A : ");
+scanf("%f",&a);
+printf("\n Enter the value of B : ");
+scanf("%f",&b);
+printf("\n Enter the value of C : ");
+scanf("%f",&c);
+
+x1=(sqrt(pow(b,2)-(4*a*c)))/(2*a);
+
+x2=-(sqrt(pow(b,2)-(4*a*c)))/(2*a);
+
+printf("\n The value of first root is : %f",x1);
+printf("\n The value of second root is : %f",x2);
+
+return 0;
+}
