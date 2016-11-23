132444ce824441d6920c09e40bb8c17ecad7a24d
Sabin koju <cabinkoju@gmail.com>
2016-11-23 12:32:24 +0545

    Add files via upload
    WPA to find ASCII Value of any character by user.

---
+++ ASCII.c
@@ -0,0 +1,30 @@
+/*
+Name : Sabin Koju
+Subject : Programming fundamental
+Lab No : 01
+Program : WPA to find ASCII Value of any character by user.
+Date :14 Nov 2016
+Roll:
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main (){
+char C;
+int a;
+printf("\n Enter any character : ");
+scanf("%c",&C);
+
+a=C;
+printf("\n\n The ASCII value of character is : %d",a);
+
+getch();
+return 0;
+}
+
+
+
+
+
