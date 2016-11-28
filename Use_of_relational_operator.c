c532cb6f57be96d8488f2daee6155350ed8bef1a
Sabin koju <cabinkoju@gmail.com>
2016-11-28 12:25:04 +0545

    WAP to use relational operator.

---
+++ Use_of_relational_operator.c
@@ -0,0 +1,44 @@
+/*
+Name : Sabin Koju
+Roll : 12
+Subject : programming fundamental
+Lab No : 07
+Program :WAP to use relational operator.
+Date : 25 Nov 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+int a,b;
+
+printf("\n Enter first number : ");
+scanf("%d",&a);
+printf("\n Enter second number : ");
+scanf("%d",&b);
+
+a==b;
+printf("\n In use of == of %d",a==b);
+
+a>b;
+printf("\n In use of > of %d",a>b);
+
+a<b;
+printf("\n In use of < of %d",a<b);
+
+a!=b;
+printf("\n In use of < of %d",a!=b);
+
+a>=b;
+printf("\n In use of >= of %d",a>=b);
+
+a<=b;
+printf("\n In use of >= of %d",a<=b);
+
+return 0;
+
+}
+
+
+
