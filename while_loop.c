dffe3bdad0420d358fb11028d84fc5b93b825293
Sabin koju <cabinkoju@gmail.com>
2017-01-02 10:29:17 +0545

    WAP to print all natural number from 1 to n using while loop.

---
+++ while_loop.c
@@ -0,0 +1,19 @@
+/*
+Name : Sabin Koju
+Roll : 33
+Lab no : 15
+subject : Programming fundamental
+Program :WAP to print all natural number from 1 to n using while loop.
+Date : 2 jan 2017
+*/
+#include<stdio.h>
+int main (){
+int n,a=1;
+printf("\n\t Enter the value of N: ");
+scanf("%d",&n);
+printf("\n\t\t Natural Numbers are.\n");
+while(a<n)
+
+printf("\n\t\t%d",a++);
+return 0;
+}
