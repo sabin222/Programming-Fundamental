6e5ee5d99ae6233379fff9bf5a0faf3f5a6cc0d9
Sabin koju <cabinkoju@gmail.com>
2017-01-15 11:07:41 +0545

    WAP to display the following output  *.

---
+++ ***.c
@@ -0,0 +1,26 @@
+/*
+ Student name : Sabin Koju
+ Subject : programming fundamental
+ Roll no : --
+ Lab sheet no : 18
+ Program : WAP to display the following output.
+ Date : 15 jan,2017
+ */
+#include<stdio.h>
+
+int main (){
+int i,j,n;
+printf("\n Enter any number : ");
+scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+{
+  for(j=1;j<=i;j++)
+  {
+	
+   printf("*");
+}
+printf("\n");
+}
+return 0;
+}
