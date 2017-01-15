dd49883f2e2915c4a18fd6baae044802a903885c
Sabin koju <cabinkoju@gmail.com>
2017-01-15 11:08:58 +0545

    WAP to display the following 54321 output.

---
+++ 321.c
@@ -0,0 +1,26 @@
+/*
+ Student name : Sabin Koju
+ Subject : programming fundamental
+ Roll no : --
+ Lab sheet no : 18
+ Program : WAP to display the following output.
+ Date : 15 jan,2017
+ */
+
+#include<stdio.h>
+
+int main (){
+int i,j;
+
+for(i=5;i>=1;i--)
+
+	{
+	for(j=1;j<=i;j++)
+	   {
+		printf("%d",j);
+		
+	   }
+	printf("\n");
+}
+return 0;
+}
