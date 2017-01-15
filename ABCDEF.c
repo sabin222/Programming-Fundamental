a460c6420cbb5889432b83f52dd2f865d67b64a5
Sabin koju <cabinkoju@gmail.com>
2017-01-15 11:09:58 +0545

    WAP to display the following A-Z output.

---
+++ ABCDEF.c
@@ -0,0 +1,25 @@
+/*
+ Student name : Sabin Koju
+ Subject : programming fundamental
+ Roll no : --
+ Lab sheet no : 18
+ Program : WAP to display the following output.
+ Date : 15 jan,2017
+ */
+
+#include<stdio.h>
+
+int main (){
+int a;
+char ch='A';
+
+
+for (a=1;a<=26;a++)
+
+{
+	
+	printf("\t %c",ch);
+	ch++;
+}
+return 0;
+}
