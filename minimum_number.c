1061a58c1218cf58a2449869985bd13b0a1828a4
Sabin koju <cabinkoju@gmail.com>
2017-01-17 11:33:59 +0545

    WAP to minimum num.

---
+++ minimum_number.c
@@ -0,0 +1,34 @@
+/*
+Name : Sabin Koju
+Roll :
+Subject : Programming fundamental
+Lab No : 19
+Program : WAP to find diameter,circumference and area of circle.
+Date : 17 jan 2017
+*/
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+void minimum();
+int main()
+{
+	minimum();
+	return 0;
+}
+void minimum()
+{
+	int a,b;
+	printf("\n Enter first number :");
+	scanf("%d",&a);
+	printf("\n Enter second number :");
+	scanf("%d",&b);
+if(a<=b)
+
+printf("\n The number %d is minimum.",a);
+
+else
+
+printf("b");
+
+}	
